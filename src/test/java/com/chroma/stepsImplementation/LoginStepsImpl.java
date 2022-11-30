8670c07e771fdfc9c221a54078b35a0e937ae402
RuthBerhane <rab497@gmail.com>
2022-11-30 12:47:08 -0500

    ..

--- src/test/java/com/chroma/stepsImplementation/LoginStepsImpl.java
+++ src/test/java/com/chroma/stepsImplementation/LoginStepsImpl.java
@@ -5,20 +5,18 @@ import com.chroma.utils.CucumberLogUtils;
 
 public class LoginStepsImpl extends PageInitializer {
 
- /**
-  * Use this method to login
-  * @param username
-  * @param password
-  */
-
+    /**
+     * Use this method to login
+     * 
+     * @param username
+     * @param password
+     */
     public static void loginCredentials(String username, String password) {
-
         loginPage.userNameTextBox.sendKeys(username);
         loginPage.passwordTextBox.sendKeys(password);
         CucumberLogUtils.logExtentScreenshot();
         CucumberLogUtils.logScreenShot();
         loginPage.signInButton.click();
-
     }
 
 }
