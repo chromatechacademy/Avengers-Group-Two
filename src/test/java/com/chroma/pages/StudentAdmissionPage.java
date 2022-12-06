66a81bc828deee6cc3ec2319dbd6c3854a76bd62
ulyanamazur <ulyanamazur@chromatechacademy.com>
2022-12-05 23:26:25 -0500

    student info 40% complete

--- src/test/java/com/chroma/pages/StudentAdmissionPage.java
+++ src/test/java/com/chroma/pages/StudentAdmissionPage.java
@@ -19,6 +19,30 @@ public class StudentAdmissionPage {
     public WebElement dateOfBirthDropDown;
     @FindBy(xpath = "(//input[@id='file'])[1]")
     public WebElement studentPhotoElement;
+    @FindBy(xpath = "//input[@id='email']")
+    public WebElement studentEmailBox;
+    @FindBy(xpath = "//select[@id='class_id']")
+    public WebElement studentClassDropDown;
+    @FindBy(xpath = "(//*[contains(text(),'SDET')])[1]")
+    public WebElement classSDET_DropDown;
+    @FindBy(xpath = "//select[@id='section_id']")
+    public WebElement classSelectioElement;
+    @FindBy(xpath = "//*[contains(text(),'Git/GitHub')]")
+    public WebElement selectGitDropDown;
+    @FindBy(xpath = "//select[@name='gender']")
+    public WebElement genderDropDown;
+    @FindBy(xpath = "//*[contains(text(),'Male')]")
+    public WebElement genderMaleDropDown;
+    @FindBy(xpath = "//select[@id='category_id']")
+    public WebElement studentCategoryDropD;
+    @FindBy(xpath = "//option[@value='54']")
+    public WebElement studentCategoryMoeDDown;
+    
+   
+    
+
+    
+   
 
 
 
--- src/test/java/com/chroma/stepDefinitions/AdmissionMtplStudentsStepDef.java
+++ src/test/java/com/chroma/stepDefinitions/AdmissionMtplStudentsStepDef.java
@@ -1,14 +1,12 @@
 package com.chroma.stepDefinitions;
 
 import javax.swing.Action;
-
 import org.apache.velocity.runtime.directive.Break;
 import org.checkerframework.checker.units.qual.s;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
-
 import com.chroma.appsCommon.PageInitializer;
 import com.chroma.pages.StudentAdmissionPage;
 import com.chroma.stepsImplementation.LoginStepsImpl;
@@ -16,15 +14,10 @@ import com.chroma.utils.Constans;
 import com.chroma.utils.CucumberLogUtils;
 import com.chroma.web.CommonUtils;
 import com.chroma.web.WebDriverUtils;
-
-import cucumber.api.java.an.Y;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import cucumber.api.java.en_lol.WEN;
-import dev.failsafe.internal.util.Assert;
-import io.opentelemetry.sdk.metrics.SdkMeterProvider;
 
 public class AdmissionMtplStudentsStepDef extends PageInitializer {
 
@@ -40,17 +33,11 @@ public class AdmissionMtplStudentsStepDef extends PageInitializer {
         LoginStepsImpl.loginCredentials(username, password);
         CucumberLogUtils.logExtentScreenshot();
         CucumberLogUtils.logScreenShot();
-
     }
 
     @Then("navigates to the Student Information dropdown module")
     public void navigates_to_the_Student_Information_dropdown_module() {
         studentInformationPage.studentInfoDropDElement.click();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     @And("selects the Student Admission option")
@@ -62,9 +49,7 @@ public class AdmissionMtplStudentsStepDef extends PageInitializer {
     @Then("enters data in {int}")
     public void enters_data_in(Integer admInteger) {
         studentAdmissionPage.admissionNoBox.click();
-        CommonUtils.sleep(3000);
         studentAdmissionPage.admissionNoBox.sendKeys(admInteger.toString());
-        CommonUtils.sleep(3000);
     }
 
     @Then("user enters number in {int}")
@@ -72,47 +57,24 @@ public class AdmissionMtplStudentsStepDef extends PageInitializer {
         studentAdmissionPage.rollNoBox.click();
         CommonUtils.sleep(3000);
         studentAdmissionPage.rollNoBox.sendKeys(rollNoInteger.toString());
-        CommonUtils.sleep(3000);
     }
 
     @Then("user enters data in Al")
     public void user_enters_data_in_Al() {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         studentAdmissionPage.firstNameBox.click();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         studentAdmissionPage.firstNameBox.sendKeys("Al".toString());
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
     }
 
     @Then("user enters data in Bo")
     public void user_enters_data_in_Bo() {
         studentAdmissionPage.firstNameBox.click();
         studentAdmissionPage.firstNameBox.sendKeys("Bo".toString());
-
     }
 
     @Then("user enters data in Ck")
     public void user_enters_data_in_Ck() {
         studentAdmissionPage.firstNameBox.click();
         studentAdmissionPage.firstNameBox.sendKeys("Ck".toString());
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     @Then("user enters data in Smith")
@@ -123,17 +85,64 @@ public class AdmissionMtplStudentsStepDef extends PageInitializer {
 
     @Then("user enters data in {string}")
     public void user_enters_data_in(String dateOfBirth) {
-
         studentAdmissionPage.dateOfBirthDropDown.click();
         studentAdmissionPage.dateOfBirthDropDown.sendKeys(dateOfBirth.toString());
         CommonUtils.sleep(3000);
     }
 
+    @Then("user enters Al@gmail.com in Emailbox")
+    public void user_enters_Al_gmail_com_in_Emailbox() {
+        CommonUtils.sleep(3000);
+        studentAdmissionPage.studentEmailBox.click();
+        CommonUtils.sleep(3000);
+        studentAdmissionPage.studentEmailBox.sendKeys("Al@gmail.com".toString());
+
+    }
+
+    @Then("user enters Bo@gmail.com in Emailbox")
+    public void user_enters_Bo_gmail_com_in_Emailbox() {
+        CommonUtils.sleep(3000);
+        studentAdmissionPage.studentEmailBox.click();
+        CommonUtils.sleep(3000);
+        studentAdmissionPage.studentEmailBox.sendKeys("Bo@gmail.com".toString());
+    }
+
+    @Then("user enters Ck@gmail.com in Emailbox")
+    public void user_enters_Ck_gmail_com_in_Emailbox() {
+        CommonUtils.sleep(3000);
+        studentAdmissionPage.studentEmailBox.click();
+        CommonUtils.sleep(3000);
+        studentAdmissionPage.studentEmailBox.sendKeys("Ck@gmail.com".toString());
+    }
+
+    @Then("user makes selection in SDET dropdown")
+    public void user_makes_selection_in_SDET_dropdown() {
+        CommonUtils.sleep(3000);
+        studentAdmissionPage.studentClassDropDown.click();
+        CommonUtils.sleep(3000);
+        studentAdmissionPage.classSDET_DropDown.click();
+    }
+
+    @Then("user makes selection in a Git\\/GitHub  dropdown")
+    public void user_makes_selection_in_a_Git_GitHub_dropdown() {
+        studentAdmissionPage.classSelectioElement.click();
+        studentAdmissionPage.selectGitDropDown.click();
+    }
+
+    @Then("user makes selection  in a male  dropdown")
+    public void user_makes_selection_in_a_male_dropdown() {
+        studentAdmissionPage.genderDropDown.click();
+        studentAdmissionPage.genderMaleDropDown.click();
+    }
+
+    @Then("user makes selection in a Moe dropdown")
+    public void user_makes_selection_in_a_Moe_dropdown() {
+        studentAdmissionPage.studentCategoryDropD.click();
+
+    }
+
     @Then("user drags Al in a box")
     public void user_drags_Al_in_a_box() {
-
-        CommonUtils.sleep(3000);
-        //studentAdmissionPage.studentPhotoElement.click();
         CommonUtils.sleep(3000);
         studentAdmissionPage.studentPhotoElement.sendKeys(Constans.studentPhoto);
         CommonUtils.sleep(3000);
@@ -141,11 +150,23 @@ public class AdmissionMtplStudentsStepDef extends PageInitializer {
 
     @Then("user drags Bo in a box")
     public void user_drags_Bo_in_a_box() {
-
+        CommonUtils.sleep(3000);
+        studentAdmissionPage.studentPhotoElement.sendKeys(Constans.studentPhoto);
+        CommonUtils.sleep(3000);
     }
 
     @Then("user drags Ck in a box")
     public void user_drags_Ck_in_a_box() {
-
+        CommonUtils.sleep(3000);
+        studentAdmissionPage.studentPhotoElement.sendKeys(Constans.studentPhoto);
+        CommonUtils.sleep(3000);
     }
+    @Then("user makes selection to a Moe dropdown")
+public void user_makes_selection_to_a_Moe_dropdown() {
+    studentAdmissionPage.studentCategoryDropD.click();
+    studentAdmissionPage.studentCategoryMoeDDown.click();
+}
+
+
+
 }
